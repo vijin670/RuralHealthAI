5d8577ccbbe281824aa3de29f75dede0b171142d
VIJIN AJAI V <vijin670@gmail.com>
2026-04-26 19:44:43 +0530

    Update eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
+export default defineConfig([ 
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
